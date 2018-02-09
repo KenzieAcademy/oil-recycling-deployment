c1b84e129ce8884bb4b89ca6e16c5ea5d363fe6d
Edwin Marshall <emarshall85@gmail.com>
2018-02-09 17:28:54 -0500

    Update run.sh
    Added a ton of comments and passed tag down to appropriate script.

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
 
+# repository this script is in
 REPO="oil-recycling-deployment"
+# which server we're building (either "frontend" or "backend"
 SERVER="$1"
+
+# which tag to use when fetching the docker image
 TAG="$2"
 TAG=${TAG:=latest}
-echo $SERVER
+
+echo "Cleaning up from previous deployment..."
+rm -rf master.zip $REPO-master
+
+# Unzip repository archive, descend into it, and launch the appropriate script
+echo "Downloading deploymnent scripts..."
 wget -c "https://github.com/KenzieAcademy/$REPO/archive/master.zip"
-unzip master.zip && cd "$REPO-master" && ./launch-$SERVER.sh
+unzip master.zip && cd "$REPO-master" && ./launch-$SERVER.sh $TAG
