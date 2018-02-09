16ba5ec4501deeb4644a23183dd5f7aa38f8cb29
Edwin Marshall <emarshall85@gmail.com>
2018-02-08 21:00:01 -0500

    Added more janky scripts.

--- launch-backend.sh
+++ launch-backend.sh
@@ -6,4 +6,4 @@ docker run --name oil-recycling-db -d mongo:latest
 # start backend container pointing to mongo container
 MONGO_HOST=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' oil-recycling-db)
 echo $MONGO_HOST
-docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 8080:8080 kenzieacademy/oil-recycling-node
+docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 8080:8080 -d kenzieacademy/oil-recycling-node
