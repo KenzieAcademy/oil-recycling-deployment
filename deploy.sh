101d4ad73f8e64788ca651544384d645e28e8566
Edwin Marshall <emarshall85@gmail.com>
2018-02-08 20:18:24 -0500

    Added script to start backend.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# start a mongodb container
+docker run --name oil-recycling-db -d mongo:latest
+
+# start backend container pointing to mongo container
+MONGO_HOST=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' oil-recycling-db)
+echo $MONGO_HOST
+docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 8080:8080 kenzieacademy/oil-recycling-node
