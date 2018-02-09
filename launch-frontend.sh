d3b6e587d33a5e6b5b4be53fc62c4a98e7fa1f52
Edwin Marshall <emarshall85@gmail.com>
2018-02-08 21:03:26 -0500

    minor correction.

--- launch-frontend.sh
+++ launch-frontend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --name oil-recycling-fe -p 8080:8080 -d kenzieacademy/oil-recycling-fe
+docker run --name oil-recycling-fe -p 8080:8080 -d kenzieacademy/oil-recycling-project-fe
