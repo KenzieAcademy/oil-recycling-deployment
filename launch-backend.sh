b4f8a84db7353220c49aeb4620272e5c94b29820
Edwin <edwin@kenzie.academy>
2018-02-09 17:30:52 -0500

    Updated some variables.

--- launch-backend.sh
+++ launch-backend.sh
@@ -6,6 +6,9 @@
 # start a mongodb container
 docker run --name oil-recycling-db -d mongo:latest
 
+# Tag of the image we want to grab
+TAG="$1"
+
 # start backend container pointing to mongo container
 MONGO_HOST=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' oil-recycling-db)
-docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 80:8080 -d kenzieacademy/oil-recycling-node
+docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 80:8080 -d kenzieacademy/oil-recycling-node:$TAG
--- launch-frontend.sh
+++ launch-frontend.sh
@@ -3,6 +3,9 @@
 # calling script to clean docker container
 ./clean-docker.sh
 
+# Tag of the image we want to grab
+TAG="$1"
+
 BACKEND_API=http://18.219.106.221
 
-docker run -e REACT_APP_OIL_RECYCLING_API=$BACKEND_API --name oil-recycling-fe -p 80:3000 -d kenzieacademy/oil-recycling-project-fe
+docker run -e REACT_APP_OIL_RECYCLING_API=$BACKEND_API --name oil-recycling-fe -p 80:3000 -d kenzieacademy/oil-recycling-project-fe:$TAG
