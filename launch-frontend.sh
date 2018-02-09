b88e2b72693f4aa351875d3066fdde7ced0f273a
Edwin <edwin@kenzie.academy>
2018-02-09 16:12:47 -0500

    added more environment variables to frontend.

--- launch-frontend.sh
+++ launch-frontend.sh
@@ -3,4 +3,6 @@
 # calling script to clean docker container
 ./clean-docker.sh
 
-docker run --name oil-recycling-fe -p 80:3000 -d kenzieacademy/oil-recycling-project-fe
+BACKEND_API=http://18.219.106.221
+
+docker run -e REACT_APP_OIL_RECYCLING_API=$BACKEND_API --name oil-recycling-fe -p 80:3000 -d kenzieacademy/oil-recycling-project-fe
