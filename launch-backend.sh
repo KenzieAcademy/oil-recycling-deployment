8ac97f659691aef7d27b10d79d7f1b3bfe448615
Edwin <edwin@kenzie.academy>
2018-03-13 16:54:29 -0400

    Added volume mounts for data to persist between deployments

--- launch-backend.sh
+++ launch-backend.sh
@@ -15,9 +15,9 @@ docker pull "$REPO/oil-recycling-node:$TAG"
 
 
 # start a mongodb container
-docker run --name oil-recycling-db -d mongo:latest
+docker run --name oil-recycling-db -v /usr/src/app/data/db:/data/db -d mongo:latest
 
 
 # start backend container pointing to mongo container
 MONGO_HOST=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' oil-recycling-db)
-docker run -e MONGO_HOST=$MONGO_HOST -v /usr/src/app/data/db:/data/db --name oil-recycling-node -p 80:8080 -d kenzieacademy/oil-recycling-node:$TAG
+docker run -e MONGO_HOST=$MONGO_HOST --name oil-recycling-node -p 80:8080 -d kenzieacademy/oil-recycling-node:$TAG
